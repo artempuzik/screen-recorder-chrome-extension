f614662d038338cd637b8a7e653abbddfa3d5d8f
Artem Puzik <artem.puzik.it@gmail.com>
2023-12-19 10:50:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+
+1. Clone this repository to your local machine
+2. Open the Google Chrome browser and go to `chrome://extensions/`.
+3. Turn on the "Developer mode" toggle in the top right corner.
+4. Click on the "Load unpacked" button and select the directory where you cloned this repository.
+5. The extension should now be loaded and ready to use.
---
+++ popup.js
@@ -0,0 +1,67 @@
+const injectCamera = async () => {
+  // inject the content script into the current page
+  const tab = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!tab) return;
+
+  const tabId = tab[0].id;
+  console.log("POPUP - inject into tab", tabId);
+  await chrome.scripting.executeScript({
+    // content.js is the file that will be injected
+    files: ["content.js"],
+    target: { tabId },
+  });
+};
+
+const removeCamera = async () => {
+  // inject the content script into the current page
+  const tab = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!tab) return;
+
+  const tabId = tab[0].id;
+  console.log("POPUP - inject into tab", tabId);
+  await chrome.scripting.executeScript({
+    // content.js is the file that will be injected
+    func: () => {
+      const camera = document.querySelector("#camera");
+      if (!camera) return;
+      document.querySelector("#camera").style.display = "none";
+    },
+    target: { tabId },
+  });
+};
+
+// check chrome storage if recording is on
+const checkRecording = async () => {
+  const recording = await chrome.storage.local.get(["recording", "type"]);
+  const recordingStatus = recording.recording || false;
+  const recordingType = recording.type || "";
+  console.log("POPUP - recording status", recordingStatus, recordingType);
+  return [recordingStatus, recordingType];
+};
+
+const updateRecording = async (type) => {
+  console.log("POPUP - start recording", type);
+
+  const recordingState = await checkRecording();
+
+  if (recordingState[0] === true) {
+    // stop recording
+    chrome.runtime.sendMessage({ type: "stop-recording" });
+    await removeCamera();
+  } else {
+    // send message to service worker to start recording
+    chrome.runtime.sendMessage({
+      type: "start-recording",
+      recordingType: type,
+    });
+    await injectCamera();
+  }
+
+  // close popup
+  //window.close();
+};
+const init = async () => {
+  await updateRecording("screen");
+};
+
+init().catch(err => console.log(err));
